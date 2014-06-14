efc59e339ece0cc5da42b04a1b8404c48234d900
MikeESL <mike.reser.03@gmail.com>
2014-06-14 16:55:52 -0400

    Recent Views Ctrl

---
+++ app/scripts/controllers/recentView.js
@@ -0,0 +1,11 @@
+angular.model('finalProjectApp')
+.controller('RecentCtrl', function($scope, $http) {
+    $http.get('ghost.json').then(
+      function(ghost){
+        $scope.ghost = ghost.data;
+      }
+    );
+    $scope.orderProp = 'age';
+    $scope.quantity = 3;
+  }
+);
